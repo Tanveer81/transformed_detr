44161185d4545b663d84ca0a5c94719a16b413b8
Tanveer81 <tanveer.hannan@campus.lmu.de>
2020-11-30 08:59:37 +0100

    First commit

--- models/detr.py
+++ models/detr.py
@@ -38,6 +38,7 @@ class DETR(nn.Module):
         self.bbox_embed = MLP(hidden_dim, hidden_dim, 4, 3)
         self.query_embed = nn.Embedding(num_queries, hidden_dim)
         self.input_proj = nn.Conv2d(backbone.num_channels, hidden_dim, kernel_size=1)
+        # add ViT backbone
         self.backbone = backbone
         self.aux_loss = aux_loss
 
