07b14b1ffc3d78545b14360150505c60cd938941
Tanveer81 <tanveer.hannan@campus.lmu.de>
2021-02-28 21:42:35 +0100

    timm added as submodule

--- models/detr.py
+++ models/detr.py
@@ -10,7 +10,7 @@ from torch import nn
 from torchvision.ops import nms
 import matplotlib.pyplot as plt
 
-from .modified_timm.timm.timm import create_model
+from timm import create_model
 from util import box_ops
 from util.misc import (NestedTensor, nested_tensor_from_tensor_list,
                        accuracy, get_world_size, interpolate,
